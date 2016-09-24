3ac2f506f5556f62f4c5190e019d8c17636a51bd
ziofat <printlts@gmail.com>
2016-09-24 22:52:19 +1000

    test: change the output since the algorithm has changed

--- test/test.js
+++ test/test.js
@@ -3,29 +3,29 @@ const utils = require('../libs/utils');
 const should = require('should');
 
 describe('Amaranth/index.js', () => {
-	it('PIN password for "world" with master pass "hello" should be 5583', () => {
+	it('PIN password for "world" with master pass "hello" should be 3599', () => {
 		const password = amaranth('hello', 'world', 'PIN');
-		should(password).be.exactly('5583');
+		should(password).be.exactly('3599');
 	});
 
-	it('Short password for "world" with master pass "hello" should be Ish6', () => {
+	it('Short password for "world" with master pass "hello" should be gsG2', () => {
 		const password = amaranth('hello', 'world', 'Short');
-		should(password).be.exactly('Ish6');
+		should(password).be.exactly('gsG2');
 	});
 
-	it('Classic password for "world" with master pass "hello" should be scR0(9Vj', () => {
+	it('Classic password for "world" with master pass "hello" should be qcq6(U0K', () => {
 		const password = amaranth('hello', 'world', 'Classic');
-		should(password).be.exactly('scR0(9Vj');
+		should(password).be.exactly('qcq6(U0K');
 	});
 
-	it('Long password for "world" with master pass "hello" should be Bla9s8EsT<-iK2q5', () => {
+	it('Long password for "world" with master pass "hello" should be Z-zzs3/t18LoAY3a', () => {
 		const password = amaranth('hello', 'world', 'Long');
-		should(password).be.exactly('Bla9s8EsT<-iK2q5');
+		should(password).be.exactly('Z-zzs3/t18LoAY3a');
 	});
 
-	it('Long password for "世界" with master pass "你好" should be U0t,Qa0!ZG6vjwv4', () => {
+	it('Long password for "世界" with master pass "你好" should be b{Cxwx3vA1OZ6,i1', () => {
 		const password = amaranth('你好', '世界', 'Long');
-		should(password).be.exactly('U0t,Qa0!ZG6vjwv4');
+		should(password).be.exactly('b{Cxwx3vA1OZ6,i1');
 	});
 
 	it('Throw error when passing unsupported password type', () => {
@@ -41,13 +41,13 @@ describe('Amaranth/libs/utils.js', () => {
 		should(idxArr).be.an.Array().and.with.lengthOf(32);
 	});
 
-	it('index array of ("hello","world") should start with 78 00 df 8a 3b', () => {
+	it('index array of ("hello","world") should start with 76 82 0e 0e d7', () => {
 		const idxArr = utils.generateIndexArray('hello', 'world');
-		should(idxArr[0]).be.exactly('78');
-		should(idxArr[1]).be.exactly('00');
-		should(idxArr[2]).be.exactly('df');
-		should(idxArr[3]).be.exactly('8a');
-		should(idxArr[4]).be.exactly('3b');
+		should(idxArr[0]).be.exactly('76');
+		should(idxArr[1]).be.exactly('82');
+		should(idxArr[2]).be.exactly('0e');
+		should(idxArr[3]).be.exactly('0e');
+		should(idxArr[4]).be.exactly('d7');
 	});
 
 	it('Template of PIN should be NNNN', () => {
@@ -59,19 +59,19 @@ describe('Amaranth/libs/utils.js', () => {
 	it('Template of Short should be CccN', () => {
 		const idxArr = utils.generateIndexArray('hello', 'world');
 		const template = utils.findTemplate(idxArr[0], 'Short');
-		should(template).be.exactly('CccN');
+		should(template).be.exactly('ccCN');
 	});
 
 	it('Template of Classic should be ccCNPNCc', () => {
 		const idxArr = utils.generateIndexArray('hello', 'world');
 		const template = utils.findTemplate(idxArr[0], 'Classic');
-		should(template).be.exactly('ccCNPNCc');
+		should(template).be.exactly('cccNPCNC');
 	});
 
 	it('Template of Long should be CccNcNCcCPPcCNcN', () => {
 		const idxArr = utils.generateIndexArray('hello', 'world');
 		const template = utils.findTemplate(idxArr[0], 'Long');
-		should(template).be.exactly('CccNcNCcCPPcCNcN');
+		should(template).be.exactly('CPcccNPcNNCcCCNc');
 	});
 });
 
